67714fd759058c5184db22a97e89fda17e96c7ca
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-03-12 14:23:20 +0100

    complete milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import { useState } from "react"
 
 function App() {
 
+  const [articoli, setArticoli] = useState([
+    "Introduzione a JavaScript",
+    "Come funziona il DOM",
+    "Guida base a React",
+    "Le arrow functions spiegate bene",
+    "Array e metodi utili in JavaScript"
+  ])
 
   return (
     <>
-
+      <ul>
+        {
+          articoli.map((articolo, index) => (
+            <li key={index}>
+              {articolo}
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
